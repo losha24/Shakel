028aa5b7ffeb48ceb32f95ca236eec20f4403048
losha24 <losha24@gmail.com>
2026-02-26 19:18:46 +0200

    Update cache version from finance-v1 to finance-v2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('finance-v1').then(cache => {
+        caches.open('finance-v2').then(cache => {
             return cache.addAll(['index.html','manifest.json','icon-192.png','icon-512.png']);
         })
     );
