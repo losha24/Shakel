e8c6fadc9989847bc08e6b5f94b2f27488136d18
losha24 <losha24@gmail.com>
2026-02-26 13:39:49 +0200

    Update service worker to version 4

--- sw.js
+++ sw.js
@@ -1,38 +1,23 @@
-const CACHE_NAME="shakel-app-v3";
-const ASSETS=[
-"./",
-"./index.html",
-"./manifest.json",
-"./icon-192.png",
-"./icon-512.png"
-];
+const CACHE_NAME="shakel-app-v4";
+const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
-self.addEventListener("install",event=>{
+self.addEventListener("install",e=>{
 self.skipWaiting();
-event.waitUntil(
-caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS))
-);
+e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
 });
 
-self.addEventListener("activate",event=>{
-event.waitUntil(
+self.addEventListener("activate",e=>{
+e.waitUntil(
 caches.keys().then(keys=>
-Promise.all(
-keys.map(key=>{
-if(key!==CACHE_NAME){
-return caches.delete(key);
-}
-})
-)
-)
-);
+Promise.all(keys.map(k=>{
+if(k!==CACHE_NAME)return caches.delete(k);
+}))
+));
 self.clients.claim();
 });
 
-self.addEventListener("fetch",event=>{
-event.respondWith(
-caches.match(event.request).then(response=>{
-return response||fetch(event.request);
-})
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(r=>r||fetch(e.request))
 );
 });
