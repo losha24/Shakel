10336dc7d7cc3e97dccd1397cdf8efeed937289e
losha24 <losha24@gmail.com>
2026-02-26 13:11:18 +0200

    Update service worker for cache management

--- sw.js
+++ sw.js
@@ -1,11 +1,38 @@
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('finance-v1').then(cache => {
-            return cache.addAll(['index.html','manifest.json','icon-192.png','icon-512.png']);
-        })
-    );
+const CACHE_NAME = "finance-app-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
